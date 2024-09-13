11b7c661a2100306ecbce3e60854cc6199303b07
CarolDianeHA <Shelby@1980gh>
2024-09-13 10:46:14 -0400

    Static and Media Files Configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,9 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = [BASE_DIR, 'static'] # Adding static files directory
+STATIC_ROOT = BASE_DIR / 'staticfiles' # Adding static files root directory
+
+MEDIA_URL = '/media/' # Adding media URL
+MEDIA_ROOT = BASE_DIR / 'media' # Adding media root directory
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
