99c2254e067f4796037e1ae16961de49d756c487
CarolDianeHA <Shelby@1980gh>
2024-09-19 11:58:27 -0400

    Adding Login Serializer using JWT.

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 
-import shortuuid
+import shortuuid # type: ignore
 
 # Custom ShortUUIDField
 class ShortUUIDField(models.CharField):
--- backend/userauths/serializer.py
+++ backend/userauths/serializer.py
@@ -1,5 +1,22 @@
 from userauths.models import Profile, User
 from rest_framework import serializers # type: ignore
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer # type: ignore
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        
+        token['full_name'] = user.full_name
+        token['username'] = user.username
+        token['email'] = user.email
+        try:
+            token['vendo_id'] = user.vendor.id
+        except:
+            token['vendo_id'] = 0
+
+        return token   
 
 
 class UserSerializer(serializers.ModelSerializer):
