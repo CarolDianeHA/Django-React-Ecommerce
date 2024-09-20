3c94ade72f4b5cdb5bb408706364e57b4f31b256
CarolDianeHA <Shelby@1980gh>
2024-09-20 17:23:39 -0400

    Adding TokenObtainPair API View (Login View).

--- backend/userauths/views.py
+++ backend/userauths/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from rest_framework_simplejwt.views import TokenObtainPairView # type: ignore
+
+from userauths.models import User, Profile
+from userauths.serializer import RegisterSerializer, UserSerializer, ProfileSerializer, MyTokenObtainPairSerializer
+
 # Create your views here.
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
