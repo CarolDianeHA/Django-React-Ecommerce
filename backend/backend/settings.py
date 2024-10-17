4dc89783b5c75ba44bac295527ea1dccf67bff22
CarolDianeHA <Shelby@1980gh>
2024-10-17 12:23:29 -0400

    Implementing API Documentation Using DRF-YASG.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     # Third party apps
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,9 +19,29 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions # type: ignore
+from drf_yasg.views import get_schema_view # type: ignore
+from drf_yasg import openapi # type: ignore
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="E-Commerce Backend APIs",
+        default_version='v1',
+        description="Documentation for E-Commerce Backend APIs",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="carol.d.hernandez@outlook.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny, )
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
+
+    # Documentation
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
